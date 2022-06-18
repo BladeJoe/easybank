0ed7d9f8730614699b8541e1529e8596673d00cc
Joe <106337992+BladeJoe@users.noreply.github.com>
2022-06-18 15:22:54 +0500

    index

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/*var burgerMenu = document.querySelector('burger-menu');
+var overlay = document.querySelector('nav-burger');
+burgerMenu.addEventListener('click', function() {
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
+*/
